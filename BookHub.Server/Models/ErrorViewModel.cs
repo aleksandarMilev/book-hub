1a3be488bf11d4527c64bcefd759b5d7f80e2764
milewSasho <mileww.sasho@abv.bg>
2024-10-28 13:19:05 +0200

    Add project files.

---
+++ BookHub.Server/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BookHub.Server.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
