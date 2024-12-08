0c4c7e3aae247c10849e7a38e118764a8f1a6942
milewSasho <mileww.sasho@abv.bg>
2024-12-08 12:20:24 +0200

    Test project

---
+++ BookHub.Server/BookHub.Server.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace BookHub.Server.Tests
+{
+    using Features.Book.Service;
+
+    public class UnitTest1
+    {
+        [Fact]
+        public void TrueShouldBeTrue() 
+            => Assert.True(true);
+    }
+}
