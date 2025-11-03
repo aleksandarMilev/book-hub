ed9dd03fbfc94515c446c3f4244758e814bb41f5
aleksandarMilev <aleksandarmilev23@gmail.com>
2025-11-03 14:41:30 +0200

    client: setup ts

--- client/src/components/common/admin-route/AdminRoute.jsx
+++ client/src/components/common/admin-route/AdminRoute.jsx
@@ -1,8 +1,8 @@
-import { useContext } from "react";
-import { Navigate } from "react-router-dom";
+import { useContext } from 'react';
+import { Navigate } from 'react-router-dom';
 
-import { UserContext } from "../../../contexts/userContext";
-import { routes } from "../../../common/constants/api";
+import { UserContext } from '../../../contexts/userContext';
+import { routes } from '../../../common/constants/api';
 
 export default function AuthenticatedRoute({ element }) {
   const { isAuthenticated, isAdmin } = useContext(UserContext);
