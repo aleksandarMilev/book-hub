7196d0efe669d4e13e78da1215e472f40decf4ed
aleksandarMilev <aleksandarmilev23@gmail.com>
2025-11-06 23:15:24 +0200

    client: app setup changes

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -6,34 +6,43 @@ import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from '@typescript-eslint/eslint-plugin';
 import tsparser from '@typescript-eslint/parser';
 import prettier from 'eslint-config-prettier';
+import importPlugin from 'eslint-plugin-import';
+import unusedImports from 'eslint-plugin-unused-imports';
 
 export default [
   { ignores: ['dist'] },
+  prettier,
   {
     files: ['**/*.{js,jsx}'],
     languageOptions: {
-      ecmaVersion: 2020,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
       globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
+      parserOptions: { ecmaFeatures: { jsx: true } },
     },
     settings: { react: { version: '18.3' } },
     plugins: {
       react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      import: importPlugin,
+      'unused-imports': unusedImports,
     },
     rules: {
       ...js.configs.recommended.rules,
       ...react.configs.recommended.rules,
       ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
-      'react/jsx-no-target-blank': 'off',
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-      ...prettier,
+      'unused-imports/no-unused-imports': 'warn',
+      'import/order': [
+        'warn',
+        {
+          groups: ['builtin', 'external', 'internal', ['parent', 'sibling', 'index'], 'type'],
+          'newlines-between': 'always',
+          alphabetize: { order: 'asc', caseInsensitive: true },
+        },
+      ],
     },
   },
   {
@@ -42,8 +51,8 @@ export default [
       parser: tsparser,
       parserOptions: {
         ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
         sourceType: 'module',
+        ecmaFeatures: { jsx: true },
         project: false,
       },
       globals: globals.browser,
@@ -54,21 +63,30 @@ export default [
       react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      import: importPlugin,
+      'unused-imports': unusedImports,
     },
     rules: {
       ...js.configs.recommended.rules,
       ...react.configs.recommended.rules,
       ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
-      ...tseslint.configs['recommended'].rules,
-      ...prettier,
+      ...tseslint.configs.recommended.rules,
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
       '@typescript-eslint/no-unused-vars': [
         'warn',
         { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
       ],
       'no-unused-vars': 'off',
-      'react/jsx-no-target-blank': 'off',
-      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'import/order': [
+        'warn',
+        {
+          groups: ['builtin', 'external', 'internal', ['parent', 'sibling', 'index'], 'type'],
+          'newlines-between': 'always',
+          alphabetize: { order: 'asc', caseInsensitive: true },
+        },
+      ],
+      'unused-imports/no-unused-imports': 'warn',
     },
   },
 ];
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,44 +1,44 @@
 import { Route, Routes } from 'react-router-dom';
-import AdminRoute from './components/common/routes/admin-route/AdminRoute';
-import ProfileRoute from './components/common/routes/profile-route/ProfileRoute';
-import ChatRoute from './components/common/routes/chat-route/ChatRoute';
-import AuthenticatedRoute from './components/common/routes/authenticated-route/AuthenticatedRoute';
 import { routes } from './common/constants/api';
-import { UserContextProvider } from './contexts/user/userContext';
-import AccessDenied from './components/common/error/AccessDenied';
-import { MessageProvider } from './contexts/message/messageContext';
-import Header from './components/common/header/Header';
 import ArticleDetails from './components/article/article-details/ArticleDetails';
-import EditArticle from './components/article/edit-article/EditArticle';
-import CreateArticle from './components/article/create-article/CreateArticle';
 import ArticleList from './components/article/article-list/ArticleList';
-import BadRequest from './components/common/error/BadRequest';
-import NotFound from './components/common/error/NotFound';
+import CreateArticle from './components/article/create-article/CreateArticle';
+import EditArticle from './components/article/edit-article/EditArticle';
 import AuthorDetails from './components/author/author-details/AuthorDetails';
 import AuthorList from './components/author/author-list/AuthorList';
 import CreateAuthor from './components/author/create-author/CreateAuthor';
 import EditAuthor from './components/author/edit-author/EditAuthor';
 import BookDetails from './components/book/book-details/BookDetails';
-import CreateBook from './components/book/create-book/CreateBook';
 import BookList from './components/book/book-list/BookList';
+import CreateBook from './components/book/create-book/CreateBook';
 import EditBook from './components/book/edit-book/EditBook';
-import ReviewList from './components/review/review-list/ReviewList';
+import ChatDetails from './components/chat/chat-details/ChatDetails';
+import ChatList from './components/chat/chat-list/ChatList';
+import CreateChat from './components/chat/create-chat/CreateChat';
+import EditChat from './components/chat/edit-chat/EditChat';
+import AccessDenied from './components/common/error/AccessDenied';
+import BadRequest from './components/common/error/BadRequest';
+import NotFound from './components/common/error/NotFound';
 import Footer from './components/common/footer/Footer';
-import ReadingList from './components/reading-list/ReadingList';
-import Home from './components/home/Home';
+import Header from './components/common/header/Header';
+import AdminRoute from './components/common/routes/admin-route/AdminRoute';
+import AuthenticatedRoute from './components/common/routes/authenticated-route/AuthenticatedRoute';
+import ChatRoute from './components/common/routes/chat-route/ChatRoute';
+import ProfileRoute from './components/common/routes/profile-route/ProfileRoute';
 import GenreDetails from './components/genre/genre-details/GenreDetails';
+import Home from './components/home/Home';
 import Login from './components/identity/login/Login';
 import Logout from './components/identity/logout/Logout';
 import Register from './components/identity/register/Register';
 import NotificationList from './components/notifications/notification-list/NotificationList';
-import ProfileList from './components/profile/profile-list/ProfileList';
-import ProfileDetails from './components/profile/profile-details/ProfileDetails';
 import CreateProfile from './components/profile/create-profile/CreateProfile';
 import EditProfile from './components/profile/edit-profile/EditProfile';
-import ChatList from './components/chat/chat-list/ChatList';
-import ChatDetails from './components/chat/chat-details/ChatDetails';
-import CreateChat from './components/chat/create-chat/CreateChat';
-import EditChat from './components/chat/edit-chat/EditChat';
+import ProfileDetails from './components/profile/profile-details/ProfileDetails';
+import ProfileList from './components/profile/profile-list/ProfileList';
+import ReadingList from './components/reading-list/ReadingList';
+import ReviewList from './components/review/review-list/ReviewList';
+import { MessageProvider } from './contexts/message/messageContext';
+import { UserContextProvider } from './contexts/user/userContext';
 
 export default function App() {
   return (
@@ -122,6 +122,7 @@ export default function App() {
           <Route path={routes.badRequest} element={<BadRequest />} />
           <Route path={routes.notFound} element={<NotFound />} />
           <Route path={routes.accessDenied} element={<AccessDenied />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
         <Footer />
       </MessageProvider>
