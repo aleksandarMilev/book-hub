5ac3451a60dc9a72382796ca43548e599db7e09c
aleksandarMilev <aleksandarmilev23@gmail.com>
2025-11-24 23:54:04 +0200

    setup tests

---
+++ server/BookHub/Common/Exceptions/DbEntityNotFoundException.spec.cs
@@ -0,0 +1,24 @@
+﻿namespace BookHub.Common.Exceptions.Tests;
+
+using Xunit;
+
+public class DbEntityNotFoundExceptionSpecs
+{
+    [Fact]
+    public void Constructor_ShouldSetTheCorrectMessage()
+    {
+        var id = 42;
+        var entityType = "Book";
+
+        var exception = new DbEntityNotFoundException<int>(entityType, id);
+
+        Assert.Equal("Book with Id: 42 was not found!", exception.Message);
+    }
+
+    [Fact]
+    public void ShouldBeOfTypeException()
+    {
+        var exception = new DbEntityNotFoundException<Guid>("Author", Guid.Empty);
+        Assert.IsType<Exception>(exception);
+    }
+}
