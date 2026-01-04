3e91a97773e1b41adda749f4b2750cb111920a78
aleksandarMilev <aleksandarmilev23@gmail.com>
2026-01-04 22:33:38 +0200

    fix client notification resource type

--- client/src/features/notification/components/last-item/utils/utils.tsx
+++ client/src/features/notification/components/last-item/utils/utils.tsx
@@ -1,24 +1,23 @@
 import { FaBook, FaUser } from 'react-icons/fa';
 
-import type { ResourceType } from '@/features/notification/components/last-item/types/resourceType.js';
 import { routes } from '@/shared/lib/constants/api.js';
 
-export const getIcon = (resourceType: ResourceType) => {
+export const getIcon = (resourceType: number) => {
   switch (resourceType) {
-    case 'Book':
+    case 0:
       return <FaBook className="notification-icon" />;
-    case 'Author':
+    case 1:
       return <FaUser className="notification-icon" />;
     default:
       return null;
   }
 };
 
-export const mapResourceRoute = (resourceType: ResourceType) => {
+export const mapResourceRoute = (resourceType: number) => {
   switch (resourceType) {
-    case 'Book':
+    case 0:
       return routes.book;
-    case 'Author':
+    case 1:
       return routes.author;
     default:
       return routes.home;
